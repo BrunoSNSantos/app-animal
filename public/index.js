5a5a29f474bf07683104cc8618a10d32c1767fb2
BrunoSNSantos <techbruno.santos@gmail.com>
2025-09-26 14:33:01 -0300

    feat: Adição de cadastro de mais características do animal

--- public/index.js
+++ public/index.js
@@ -18,17 +18,23 @@ async function carregar_animais() {
 function manipular_formulario() {
   const form_animal = document.getElementById("form-animal");
   const input_nome = document.getElementById("nome");
+  const input_idade = document.getElementById("idade");
+  const input_sexo = document.getElementById("sexo");
+  const input_cor = document.getElementById("cor");
 
   form_animal.onsubmit = async (event) => {
     event.preventDefault();
     const nome_animal = input_nome.value;
+    const idade_animal = input_idade.value;
+    const sexo_animal = input_sexo.value;
+    const cor_animal = input_cor.value;
     alert(`${nome_animal} adicionado com sucesso!!!`);
 
     await axios.post("http://localhost:8000/animais", {
       nome: nome_animal,
-      idade: 4,
-      sexo: "Macho",
-      cor: "Branco",
+      idade: idade_animal,
+      sexo: sexo_animal,
+      cor: cor_animal,
     });
     carregar_animais();
   };
